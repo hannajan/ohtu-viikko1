d793e8ee43af93bdf86f8c065bf432a2a4befa57
Hanna Jantunen <hannajant@gmail.com>
2019-11-06 13:54:39 +0200

    codeclimate testailua

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,9 +9,7 @@ public class Main {
             System.out.println("kolme sisäkkäistä iffiä");
         }
         for(int i = 0; i < 1; i++) {
-            for(int j = 0; j < 1; j++) {
                 System.out.println("kaksi sisäkkäistä foria");
-            }
         }
     }
 
